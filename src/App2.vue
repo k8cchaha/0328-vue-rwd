bdc3683fbd4a73a14bd3e2b05875aef2a9058667
Alex_Wang <k8cchaha@gmail.com>
2022-03-28 16:25:17 +0800

    add components

--- src/App2.vue
+++ src/App2.vue
@@ -1,9 +1,43 @@
 <template>
-  <h1>Hello Vue</h1>
+  <Header />
+  <Article />
+  <Aside />
+  <Main />
+  <Footer />
 </template>
 
 <script>
-export default {};
+import Header from "./components/Header.vue";
+import Article from "./components/Article.vue";
+import Aside from "./components/Aside.vue";
+import Main from "./components/Main.vue";
+import Footer from "./components/Footer.vue";
+export default {
+  components: {
+    Header,
+    Article,
+    Aside,
+    Main,
+    Footer,
+  },
+};
 </script>
 
-<style></style>
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  box-sizing: border-box;
+  font-family: "Microsoft JhengHei", "Heiti TC", "sans-serif";
+}
+img {
+  display: block;
+}
+html,
+body {
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ src/components/Article.vue
@@ -0,0 +1,40 @@
+<template>
+  <article>
+    <div>
+      <h3>Responsive Web Design</h3>
+      <p>
+        選用對的html5標籤除了在閱讀 html
+        原始碼更加順暢外，也有助於google搜尋引擎排名順序，SEO效果的提升
+      </p>
+    </div>
+  </article>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+article {
+  width: 100%;
+  height: 578px;
+  background-image: url("~@/assets/images/banner.png");
+  background-size: cover;
+
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+}
+article > div > h3 {
+  text-align: center;
+  font-size: 48px;
+  color: #fff;
+}
+article > div > p {
+  text-align: center;
+  font-size: 14px;
+  color: #fff;
+  line-height: 3em;
+}
+</style>
